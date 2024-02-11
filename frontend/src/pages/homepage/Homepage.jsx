20e56758cab7e91ecf267d899fea026a45be4755
Bobby <bobbypatel94@gmail.com>
2024-02-11 18:24:36 +0530

    backend resilve

--- frontend/src/pages/homepage/Homepage.jsx
+++ frontend/src/pages/homepage/Homepage.jsx
@@ -5,25 +5,43 @@ import "./Homepage.css"
 import PostCard from "../../components/postCard/PostCard"
 
 function Homepage() {
+
+  const getAllData_API = "http://localhost//story";
+
   return (
     <div className='homepage'>
+      
       <Navbar />
 
+      fetch(getAllData_API)
+        .then(response => {
+
+          if(!response.ok){
+            throw new Error("network response was not ok");
+          }
+
+          return response.json();
+
+        })
+        .then(data=>{
+            <div className='homepage-content'>
+              {postdata.map((data, index) => (
+                <div className='grid-item'>
+                <PostCard
+                  key={index}
+                  username={data.username}
+                  date={data.date}
+                  text={data.text}
+                />
+                </div>
+              ))}
+          </div>
+        })
+        .catch(error => {
+          console.error("there was a problem with fetch opereation" , error);
+        })
 
       
-
-      <div className='homepage-content'>
-        {postdata.map((data, index) => (
-          <div className='grid-item'>
-          <PostCard
-            key={index}
-            username={data.username}
-            date={data.date}
-            text={data.text}
-          />
-          </div>
-        ))}
-      </div>
     </div>
   )
 }

6b1e398b2802ef0c8e43c866c71520877a2ff8b4
Bobby <bobbypatel94@gmail.com>
2024-02-15 16:58:10 +0530

    delete post functionality added

--- frontend/src/components/admin/manage-post/ManagePost.jsx
+++ frontend/src/components/admin/manage-post/ManagePost.jsx
@@ -30,37 +30,10 @@ function ManagePost() {
     }
   };
 
-  const handleDelete = (id) => {
-    fetch(`http://localhost:5000/story/${id}`, {
-      method: 'DELETE',
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log('Post deleted:', data);
-       
-        // onDelete(id); // Update UI to remove the deleted post
-      })
-      .catch(error => {
-        console.error('Error deleting post:', error);
-      });
-    // // Remove the item with the specified id from the data
-    // const updatedData = tableData.filter((item) => item._id !== id);
-    // // console.log(_id);
-    // setTableData(updatedData);
-  };
-
-
-
-
   return (
     <div className='manage-post'>
       <h2>Manage Post</h2>
-      <Table data={tableData} onDelete={handleDelete} />
+      <Table data={tableData}  />
     </div>
   )
 };
--- frontend/src/components/admin/manage-post/Table.jsx
+++ frontend/src/components/admin/manage-post/Table.jsx
@@ -3,7 +3,7 @@ import './Table.css';
 import ModalComponent from '../modal/Modal';
 
 
-const Table = ({ data, onDelete }) => {
+const Table = ({ data }) => {
 
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [selectedUser, setSelectedUser] = useState({});
@@ -16,6 +16,27 @@ const Table = ({ data, onDelete }) => {
       const closeModal = () => {
         setModalIsOpen(false);
       };
+
+    const handleDelete = (item) =>{
+        const id = item._id;
+       
+        const delete_API = `http://localhost:5000/story/${id}`;
+
+        fetch(delete_API,{method:'DELETE',})
+        .then(response=>{
+            if(!response.ok){
+               throw new Error('Network response was not ok!');
+            }
+
+            return response.json();
+        })
+        .then(data=>{
+            console.log('Post deleted:', data);
+        })
+        .catch(error => {
+            console.error('Error deleting post:', error);
+          });
+    }
 
     return (
         <div className='manage-table'>
@@ -30,13 +51,13 @@ const Table = ({ data, onDelete }) => {
                 </thead>
                 <tbody>
                     {data.map((item) => (
-                        <tr key={item.id}>
+                        <tr key={item._id}>
                             <td>{item.name}</td>
                             <td>{item.text}</td>
                             <td>{item.likes}</td>
                             <td className='tableButtons'>
                                 <button onClick={() => handleRead(item)} className='read'>Read</button>
-                                <button onClick={() => onDelete(item._id)} className='delete'>Delete</button>
+                                <button onClick={() => handleDelete(item)} className='delete'>Delete</button>
                             </td>
                         </tr>
                     ))}
